161023bb68a9367a31878e5a0e4be458cb2e1ac9
AWS-Janghwan <janghwan@amazon.com>
2024-11-15 10:42:42 +0900

    CI/CD Script edited

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-sh run.sh
+
+# 애플리케이션 디렉토리로 이동
+cd /data/AWS-Demo-Factory
+
+
+# 서버 시작
+lsof -i | grep node |kill -9 `awk '{print $2}'`
+nohup npm run start & 
+sleep 10
+echo "Deployment completed successfully."
+
+
+# if [ "$NODE_ENV" == "production" ]; then
+#     npm run start
+# else
+#     npm run dev
+# fi
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
 
-# 애플리케이션 디렉토리로 이동
-cd /data/AWS-Demo-Factory
-
-
-# 서버 시작
-lsof -i | grep node |kill -9 `awk '{print $2}'`
-nohup npm run start & 
-sleep 10
-echo "Deployment completed successfully."
-
-
-# if [ "$NODE_ENV" == "production" ]; then
-#     npm run start
-# else
-#     npm run dev
-# fi
+sh run.sh
