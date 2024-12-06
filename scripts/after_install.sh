c1f724c72400f3229a08097c1dfd5bd34f8a903d
AWS-Janghwan <janghwan@amazon.com>
2024-12-06 19:47:14 +0900

    revision scripts

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -17,7 +17,7 @@
 cd /data/AWS-Demo-Factory
 
 # # npm 패키지 설치
-npm install
+npm install --force
 
 # # 리액트 앱 빌드
 # npm run build
@@ -30,7 +30,7 @@ fi
 cp -r /data/video/* /data/AWS-Demo-Factory/public/source/video/
 
 # 권한 재설정
-sudo chown -R root:ec2-user /data/AWS-Demo-Factory
-sudo chmod -R 775 /data/AWS-Demo-Factory
+#sudo chown -R root:ec2-user /data/AWS-Demo-Factory
+#sudo chmod -R 775 /data/AWS-Demo-Factory
 
 echo "Deployment completed successfully"
