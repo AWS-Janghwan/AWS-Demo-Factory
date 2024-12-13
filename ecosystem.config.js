bfbbabc41153a558a227c65317e47dc8d238c0b7
AWS-Janghwan <janghwan@amazon.com>
2024-12-13 20:21:33 +0900

    Demo Update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,11 @@ module.exports = {
       PM2_SERVE_PATH: './build',
       PM2_SERVE_PORT: 80,
       PM2_SERVE_SPA: 'true',
-      PM2_SERVE_HOMEPAGE: '/index.html'
+      PM2_SERVE_HOMEPAGE: '/index.html',
+      PM2_SERVE_MIME: {
+        '.mp4': 'video/mp4'
+      }
     }
   }]
 }
+
